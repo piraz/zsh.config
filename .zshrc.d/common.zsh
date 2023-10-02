9b48c25889cb245cb86615e33bfacd428694215b
Flavio Garcia <piraz@candango.org>
2023-10-02 12:23:49 -0400

    feat(common): add aliases to git, kubectl, and tmux

--- .zshrc.d/common.zsh
+++ .zshrc.d/common.zsh
@@ -2,7 +2,8 @@
 export GOPATH=$HOME/go
 
 # Aliases
-alias ls="ls --color"
+alias k="kubectl"
+alias g="git"
 alias gad="git add"
 alias gbr="git branch"
 alias gch="git checkout"
@@ -11,3 +12,5 @@ alias gdc="git diff --cached"
 alias gst="git status"
 alias gpl="git pull"
 alias gps="git push"
+alias ls="ls --color"
+alias t="tmux"
