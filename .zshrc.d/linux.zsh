1a635f529b0251f269d89b9f56951814b511f7b1
Flavio Garcia <piraz@candango.org>
2026-01-07 22:42:46 -0500

    feat(linux): add aliases for pbcopy and pbpaste

--- .zshrc.d/linux.zsh
+++ .zshrc.d/linux.zsh
@@ -1,10 +1,12 @@
-alias wezterm="flatpak run org.wezfurlong.wezterm"
+alias pbcopy="xsel --clipboard --input"
+alias pbpaste="xsel --clipboard --output"
+# alias wezterm="flatpak run org.wezfurlong.wezterm"
 
 . "$HOME/.asdf/asdf.sh"
 # append completions to fpath
 fpath=(${ASDF_DIR}/completions $fpath)
 # initialise completions with ZSH's compinit
 autoload -Uz compinit && compinit
-NVIM_PATH="/opt/nvim/current"
 export PATH=$GOPATH/bin:$NVIM_PATH/bin:$HOME/bin:$PATH:/usr/local/bin
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
+. "$HOME/.cargo/env"
